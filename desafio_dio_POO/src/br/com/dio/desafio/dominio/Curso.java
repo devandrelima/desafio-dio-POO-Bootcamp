484bf38563ab79a116d2042a3122bd9b10d3b667
devAndreLA <andre.alves@alunos.ufersa.edu.br>
2024-06-11 17:14:26 -0300

    nova classe para implementar herança

--- desafio_dio_POO/src/br/com/dio/desafio/dominio/Curso.java
+++ desafio_dio_POO/src/br/com/dio/desafio/dominio/Curso.java
@@ -1,29 +1,9 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-	private String titulo;
-	private String descricao;
+public class Curso extends Conteudo {
+
 	private int cargaHoraria;
 	
-	public int calcularXp() {
-		return 0;
-	}
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
 
 	public int getCargaHoraria() {
 		return cargaHoraria;
@@ -35,8 +15,11 @@ public class Curso {
 
 	@Override
 	public String toString() {
-		return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+		return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
 	}
-	
-	
+
+	@Override
+	public double calcularXP() {
+		return XP_PADRAO * cargaHoraria;
+	}
 }
