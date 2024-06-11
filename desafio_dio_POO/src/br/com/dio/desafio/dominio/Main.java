89d1c14b218ec3e790ed93daff0ddcc49df1661b
devAndreLA <andre.alves@alunos.ufersa.edu.br>
2024-06-11 17:15:35 -0300

    add herança com conteudo

--- desafio_dio_POO/src/br/com/dio/desafio/dominio/Main.java
+++ desafio_dio_POO/src/br/com/dio/desafio/dominio/Main.java
@@ -3,7 +3,8 @@ package br.com.dio.desafio.dominio;
 public class Main {
 	public static void main(String[] args) {
 		Curso curso = new Curso();
-		
+		Conteudo conteudo1 = new Curso();
+		Conteudo conteudo2 = new Mentoria();
 		
 	}
 }
